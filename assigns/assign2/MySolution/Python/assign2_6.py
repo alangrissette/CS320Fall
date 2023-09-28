5281d875d8f25658480bcb7f99e2908ac3b7bf67
alangrissette <112966448+alangrissette@users.noreply.github.com>
2023-09-27 21:59:20 -0400

    hw

--- assigns/assign2/MySolution/Python/assign2_6.py
+++ assigns/assign2/MySolution/Python/assign2_6.py
@@ -110,20 +110,31 @@ def string_merge(cs1, cs2):
     n1 = string_length(cs1)
     n2 = string_length(cs2)
 
-    def foreach(i1, i2, work):
-        if i1 < n1:
-            if i2 < n2:
-                c1 = string_get_at(cs1, i1)
-                c2 = string_get_at(cs2, i2)
-                if c1 <= c2:
-                    work(c1)
-                    foreach(i1 + 1, i2, work)
-                else:
-                    work(c2)
-                    foreach(i1, i2 + 1, work)
-            else:
-                int1_foreach(n1 - i1, lambda i: work(string_get_at(cs1, i1 + i)))
-        else:
-            int1_foreach(n2 - i2, lambda i: work(string_get_at(cs2, i2 + i)))
+    result = []
 
-    return string_make_fwork(lambda work: foreach(0, 0, work))
+    def work(c):
+        result.append(c)
+
+    i1 = 0
+    i2 = 0
+
+    while i1 < n1 and i2 < n2:
+        c1 = string_get_at(cs1, i1)
+        c2 = string_get_at(cs2, i2)
+
+        if c1 <= c2:
+            work(c1)
+            i1 += 1
+        else:
+            work(c2)
+            i2 += 1
+
+    while i1 < n1:
+        int1_foreach(n1 - i1, lambda i: work(string_get_at(cs1, i1 + i)))
+        break
+
+    while i2 < n2:
+        int1_foreach(n2 - i2, lambda i: work(string_get_at(cs2, i2 + i)))
+        break
+
+    return string_make_fwork(lambda work: list(map(work, result)))
